fc1482255a3eadecdd37dc214a0272dffd4b4783
MattDirty <matt@le-dahu-studio.ch>
2022-03-11 23:35:05 +0100

    fuck this

---
+++ compile.bat
@@ -0,0 +1,2 @@
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe srcs/shaders/shader.vert -o srcs/shaders/spv/vert.spv
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe srcs/shaders/shader.frag -o srcs/shaders/spv/frag.spv
