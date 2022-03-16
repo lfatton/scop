8cf6fc7c70717ded2f75d838bdd2f1b64fe5e50e
lfatton <louise.fatton@hotmail.fr>
2022-03-16 18:40:42 +0100

    refactor bis

--- includes/conf.h
+++ includes/conf.h
@@ -1,11 +1,15 @@
+#ifndef CONF_H
+# define CONF_H
 
 int const WINDOW_W = 800;
 int const WINDOW_H = 600;
-#ifdef darwin
-const char* const const OS = "ios";
 
-#elif unix
+# ifdef darwin
+const char* const const OS = "ios";
+# elif unix
 const char* const OS = "unix";
-#elif _WIN32
+# elif _WIN32
 const char* const const OS = "windows";
+# endif
+
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,47 +1,20 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <conf.h>
+#include <scop.h>
 
-void error_scop(char *error)
-{
-    std::cout << error <<std::endl;
-    exit(EXIT_FAILURE);
-}
-
-
-GLFWwindow* init_glfw()
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-
-    if (OS == "unix" || "windows")
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    else
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(WINDOW_W, WINDOW_H, "scop", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        error_scop("Failed to create GLFW window!");
+void render_loop(GLFWwindow* window) {
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-
-    glfwMakeContextCurrent(window);
-    return window;
 }
 
-void init_glad()
-{
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        error_scop("Failed to initialize GLAD!");
-}
+int main() {
+    Environment environment;
 
-int main()
-{
-    init_glfw();
-    init_glad();
+    GLFWwindow* window = environment.init_glfw();
+    environment.init_glad();
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    render_loop(window);
 
+    environment.quit();
     return 0;
 }
