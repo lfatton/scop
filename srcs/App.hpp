8124104da1868f4eac897e23fa9f61314a11a53a
lfatton <louise.fatton@hotmail.fr>
2022-11-15 17:47:32 +0100

    wip camera rotATION

--- srcs/App.hpp
+++ srcs/App.hpp
@@ -3,12 +3,20 @@
 
 # include <cmath>
 # include <algorithm>
+
+# include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
 # include "Environment.hpp"
 # include "Shader.hpp"
 # include "Vertex.hpp"
 # include "Vector.hpp"
 # include "Matrix.hpp"
 # include "Camera.hpp"
+
+static Camera camera;
 
 class App {
 private:
@@ -17,7 +25,7 @@ private:
     float mSpeedVariation{};
 
     void renderLoop(GLFWwindow* window, unsigned int shaderProgram, const Vertex& vertex);
-    void processInput(GLFWwindow* window, Camera &camera);
+    void processInput(GLFWwindow* window);
     static void quit();
 
 public:
--- srcs/Camera.cpp
+++ srcs/Camera.cpp
@@ -1,6 +1,7 @@
 #include "Camera.hpp"
 
-Camera::Camera(Vector &givenPosition, Vector &givenTarget) {
+Camera::Camera(Vector &givenPosition, Vector &givenTarget, float givenZoom) {
+    zoomValue = givenZoom;
     position = givenPosition;
     target = givenTarget;
 
@@ -14,3 +15,68 @@ Camera::Camera(Vector &givenPosition, Vector &givenTarget) {
 }
 
 Camera& Camera::operator= (const Camera &cameraToCopy) = default;
+
+void Camera::update() {
+    Camera updatedCam(this->position, this->target, this->zoomValue);
+    *this = updatedCam;
+}
+
+void Camera::move(Direction direction, float deltaTime) {
+    float velocity = this->speedValue * deltaTime;
+    velocity = 0.05f;
+    Vector right = Vector::getCrossProduct(this->target, this->yAxis);
+    right.normalise();
+
+    if (direction == FORWARD)
+        this->position = this->position + this->target * velocity;
+    if (direction == BACKWARD)
+        this->position = this->position - this->target * velocity;
+    if (direction == LEFT)
+        this->position = this->position - right * velocity;
+    if (direction == RIGHT)
+        this->position = this->position + right * velocity;
+    this->update();
+}
+
+void Camera::rotate(float xPos, float yPos) {
+    if (mFirstTime)
+    {
+        mLastX = xPos;
+        mLastY = yPos;
+        mFirstTime = false;
+    }
+
+    float xOffset = xPos - mLastX;
+    float yOffset = mLastY - yPos;
+    mLastX = xPos;
+    mLastY = yPos;
+
+    xOffset *= SENSITIVITY;
+    yOffset *= SENSITIVITY;
+
+    yaw   += xOffset;
+    pitch += yOffset;
+
+    if(pitch > PITCH_LIMIT)
+        pitch = PITCH_LIMIT;
+    if(pitch < -PITCH_LIMIT)
+        pitch = -PITCH_LIMIT;
+
+    Vector direction;
+    direction.x = cosf(convertToRadians(yaw)) * cosf(convertToRadians(pitch));
+    direction.y = sinf(convertToRadians(pitch));
+    direction.z = sinf(convertToRadians(yaw)) * cosf(convertToRadians(pitch));
+    direction.normalise();
+    this->target = direction;
+    this->update();
+}
+
+void Camera::zoom(float yOffset)
+{
+    this->zoomValue -= yOffset;
+
+    if (this->zoomValue < 1.f)
+        this->zoomValue = 1.f;
+    if (this->zoomValue > FOV)
+        this->zoomValue = FOV;
+}
